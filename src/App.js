fd42cf6ab7b27f9ce7521482f8fe7e6c2400f995
Giovanni A. Camacho <gcamacho79@gmail.com>
2020-10-09 17:06:43 -0400

    Linking the header icon to the homepage

--- src/App.js
+++ src/App.js
@@ -4,25 +4,30 @@ import './App.css';
 import MemberList from './MemberList/MemberList';
 import Member from './Member/Member';
 import { PageHeader, Layout, Divider, Affix } from 'antd';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const { Header, Content } = Layout;
 
 function App() {
   return (
     <Layout>
-      <Affix>
-        <Header>
-          <PageHeader
-            className="site-page-header"
-            title={<div><img alt="dome" src="/dome.svg" /><span>Members of the United States Congress</span></div>}
-            subTitle="Find more information about the Members of the 116th United States Congress"
-          />
-        </Header>
-      </Affix>
-      <Content style={{ padding: '0 24px 24px', marginTop: '20px' }}>
-        <Divider />
-          <Router>
+      <Router>
+        <Affix>
+          <Header>
+            <PageHeader
+              className="site-page-header"
+              title={
+                <div>
+                  <Link to={'/'}><img alt="dome" src="/dome.svg" /></Link>
+                  <span>Members of the United States Congress</span>
+                </div>
+              }
+              subTitle="Find more information about the Members of the 116th United States Congress"
+            />
+          </Header>
+        </Affix>
+        <Content style={{ padding: '0 24px 24px', marginTop: '20px' }}>
+          <Divider />
             <Switch>
               <Route exact={true} path={[ "/", "/member-list" ]}>
                 <MemberList />
@@ -31,8 +36,8 @@ function App() {
                 <Member />
               </Route>
             </Switch>
-          </Router>
-      </Content>
+        </Content>
+      </Router>
     </Layout>
   );
 }
